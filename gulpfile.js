45bb248675608fddd634ff11727c5e7447b86619
Perkovec <perkovec24@gmail.com>
2016-07-06 13:15:21 +0300

    Add gulp task

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var eslint = require('gulp-eslint');
+var uglify = require('gulp-uglify');
+var buildFiles = require('./build_list');
+
+gulp.task('build-dev', function() {
+  gulp.src(buildFiles)
+      .pipe(eslint())
+      .pipe(eslint.format())
+      .pipe(eslint.failOnError())
+      .pipe(concat('nauper-dev.js'))
+      .pipe(gulp.dest('dist'));
+});
+
+gulp.task('build-release', function() {
+  gulp.src(buildFiles)
+      .pipe(eslint())
+      .pipe(eslint.format())
+      .pipe(eslint.failOnError())
+      .pipe(uglify())
+      .pipe(concat('nauper-dev.js'))
+      .pipe(gulp.dest('dist'));
+});
+
+gulp.task(['build-dev', 'build-release']);
