efaf04fdd8e5d1772103b97fd7595c6958af6861
arseniypetrikor <arseniy.beresnev@gmail.com>
2016-12-08 17:19:05 +0500

    Utils changes
    - Added makeRequestSync function to utils

--- src/utils.js
+++ src/utils.js
@@ -105,3 +105,10 @@ let makeRequest = (url, callback) => { //eslint-disable-line
   });
   xhr.send();
 };
+
+let makeRequestSync = (url, callback) => { //eslint-disable-line
+  let xhr = new XMLHttpRequest();
+  xhr.open('GET', url, false);
+  xhr.send();
+  callback(xhr);
+};
