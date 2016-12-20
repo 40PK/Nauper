c91b7592a84f4a5126e5b3bef72c12ce4ccc0984
arseniypetrikor <arseniy.beresnev@gmail.com>
2016-12-20 21:51:42 +0500

    Character changes

--- src/character.js
+++ src/character.js
@@ -1,7 +1,9 @@
 /* global Nauper */
-Nauper.Character = function Character(emotionPaths, name, id, color) {
+Nauper.Character = function Character(engine, emotionPaths, name, id, color) {
+  this.engine = engine;
   this.emotions = emotionPaths;
   this.name = name;
   this.color = color;
   this.id = id;
+  this.engine.characters[this.id] = this;
 };
