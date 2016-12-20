e04092290d5e9dde5773c15446c0acc9079acd2b
arseniypetrikor <arseniy.beresnev@gmail.com>
2016-12-20 22:12:08 +0500

    Character changes

--- src/character.js
+++ src/character.js
@@ -6,4 +6,14 @@ Nauper.Character = function Character(engine, emotionPaths, name, id, color) {
   this.color = color;
   this.id = id;
   this.engine.characters[this.id] = this;
+
+  this.createImage();
+};
+
+Nauper.Character.prototype.createImage = function createImage() {
+  this.image = document.createElement('img');
+  this.image.id = this.id;
+  this.image.classList.add('character');
+  this.image.style.display = 'none';
+  document.body.appendChild(this.image);
 };
